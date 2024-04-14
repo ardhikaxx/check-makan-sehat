d6972f633f95994dd5763c85c8014c642611fa01
ardhikaxx <ardhikayanuar58@gmail.com>
2024-04-15 00:45:56 +0700

    add-API Key Food

---
+++ js/api-chek.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('.btn-primary').addEventListener('click', function () {
+        swal({
+            text: 'Masukkan nama makanan dalam bahasa Inggris:',
+            content: "input",
+            button: {
+                text: "Cek",
+                closeModal: false,
+            },
+        })
+        .then((inputValue) => {
+            if (inputValue === "") {
+                swal("Anda harus memasukkan makanan!", {
+                    icon: "error",
+                });
+                return;
+            }
+
+            fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${inputValue}&api_key=qyV60JbMIVfjPB0QeLnQ90vGKtmoe0UQjzigyLdS`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.foods.length === 0) {
+                    swal("Makanan tidak ditemukan!", {
+                        icon: "error",
+                    });
+                } else {
+                    const foodItem = data.foods[0];
+                    const nutrition = foodItem.foodNutrients;
+
+                    let nutrientValue = "";
+                    nutrition.forEach(nutrient => {
+                        const value = nutrient.value || 0;
+                        const unit = nutrient.unit || "";
+                        nutrientValue += `${nutrient.nutrientName}: ${value}${unit}\n`;
+                    });
+
+                    swal({
+                        title: "Hasil Cek Nutrisi",
+                        text: `Makanan "${foodItem.description}" memiliki nilai gizi:\n${nutrientValue}`,
+                        icon: "success",
+                    });
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                swal("Terjadi kesalahan saat memproses permintaan.", {
+                    icon: "error",
+                });
+            });
+        });
+    });
+});
